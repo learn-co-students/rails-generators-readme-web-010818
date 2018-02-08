d1db125bd0fff029a463307867a0f1ac3b7b9b5c
krdiamond <github email address>
2018-02-07 21:19:18 -0500

    Done.

--- db/migrate/20151230232412_create_posts.rb
+++ db/migrate/20151230232412_create_posts.rb
@@ -3,7 +3,9 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :description
-
+      t.integer :category_id
+      t.string :published_status
+      t.string :post_status
       t.timestamps null: false
     end
   end
